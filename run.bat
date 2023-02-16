4f1c80a60c2dee2eb18e626e1db6136775664723
Marko Matore <markomatore@hotmail.com>
2023-02-16 13:14:13 +0100

    bat file

--- run.bat
+++ run.bat
@@ -1,5 +1,9 @@
 @echo off
 
-call C:/Users/marmatore/Anaconda3/Scripts/activate
-call activate py39
-call C:/Users/marmatore/Anaconda3/envs/py39/python.exe C:/Users/marmatore/Desktop/GitHub/haptik_gui/gui.py
+if %USERNAME% == marmatore (
+    call C:/Users/marmatore/Anaconda3/Scripts/activate 
+    call activate py39 
+    call C:/Users/marmatore/Anaconda3/envs/py39/python.exe C:/Users/marmatore/Desktop/GitHub/haptik_gui/gui.py
+    ) else (
+        msg * "Start the GUI with terminal and command: python gui.py"
+        )
