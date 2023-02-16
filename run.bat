72550ef2ba3f77c6a37425604a7fbe53ba3ad072
Marko Matore <markomatore@hotmail.com>
2023-02-16 12:55:37 +0100

    bat file added

---
+++ run.bat
@@ -0,0 +1,5 @@
+@echo off
+
+call C:/Users/marmatore/Anaconda3/Scripts/activate
+call activate py39
+call C:/Users/marmatore/Anaconda3/envs/py39/python.exe C:/Users/marmatore/Desktop/GitHub/haptik_gui/gui.py
